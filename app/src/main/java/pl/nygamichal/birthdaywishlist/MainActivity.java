69b61f3f47b43e318e87cbf85b7d72d9ef6d8e40
nygamichal <mnyga94@gmail.com>
2017-08-30 01:26:00 +0200

    7,2

--- app/src/main/java/pl/nygamichal/birthdaywishlist/MainActivity.java
+++ app/src/main/java/pl/nygamichal/birthdaywishlist/MainActivity.java
@@ -1,13 +1,37 @@
 package pl.nygamichal.birthdaywishlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getCanonicalName();
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        realm = Realm.getDefaultInstance();
+    }
+
+    @OnClick(R.id.fabAdd)
+    public void onClickFab(){
+        Intent intent = new Intent(MainActivity.this,WishAddActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        RealmResults<Wish> wishes = realm.where(Wish.class).findAll();
+        Log.d(TAG, "onResume: ");
     }
 }
--- app/src/main/java/pl/nygamichal/birthdaywishlist/WishAddActivity.java
+++ app/src/main/java/pl/nygamichal/birthdaywishlist/WishAddActivity.java
@@ -2,12 +2,39 @@ package pl.nygamichal.birthdaywishlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.realm.Realm;
 
 public class WishAddActivity extends AppCompatActivity {
+    @BindView(R.id.editTextContent)
+    EditText editTextContent;
+    @BindView(R.id.editTextPerson)
+    EditText editTextPerson;
+    private Wish wish;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_add);
+        ButterKnife.bind(this);
+        realm = Realm.getDefaultInstance();
+    }
+
+    @OnClick(R.id.fab)
+    public void onClickFab(){
+        String content = editTextContent.getText().toString();
+        Person person = new Person(editTextPerson.getText().toString());
+        wish = new Wish(content,person,null);
+        realm.beginTransaction();
+        realm.copyToRealm(wish);
+        realm.commitTransaction();
+        Toast.makeText(this,"Saved",Toast.LENGTH_SHORT).show();
+        WishAddActivity.this.finish();
     }
 }
